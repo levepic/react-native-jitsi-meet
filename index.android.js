3e117cd26c0006d1e1198e2c32a33219e920fe88
Levente Lorch <lorchlevente@gmail.com>
2021-04-12 18:53:13 +0200

    android options

--- index.android.js
+++ index.android.js
@@ -8,14 +8,16 @@ export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
 export const JitsiMeetModule = NativeModules.RNJitsiMeetModule
 const call = JitsiMeetModule.call;
 const audioCall = JitsiMeetModule.audioCall;
-JitsiMeetModule.call = (url, userInfo) => {
-  userInfo = userInfo || {};
-  call(url, userInfo);
-}
-JitsiMeetModule.audioCall = (url, userInfo) => {
-  userInfo = userInfo || {};
-  audioCall(url, userInfo);
-}
+JitsiMeetModule.call = (url, userInfo, params) => {
+    userInfo = userInfo || {};
+    params = params || {};
+    call(url, userInfo, params);
+  }
+  JitsiMeetModule.audioCall = (url, userInfo, params) => {
+    userInfo = userInfo || {};
+    params = params || {};
+    audioCall(url, userInfo, params);
+  }
 export default JitsiMeetModule;
 
 
